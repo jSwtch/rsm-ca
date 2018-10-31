fa1bedc3925c4adb2880bff84bda906dd457d9f1
Jonathan Sanderson <sanderson.jma@gmail.com>
2018-10-31 15:25:20 -0600

    inital commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,21 +2,3 @@ import anime from 'animejs'
 import Rellax from 'rellax'
 
 var rellax = new Rellax('.rellax');
-
-
-anime({
-  targets: '.test',
-  translateX: [{
-      value: 100,
-      duration: 1200
-    },
-    {
-      value: 0,
-      duration: 800
-    }
-  ],
-  rotate: '1turn',
-  backgroundColor: '#FFF',
-  duration: 2000,
-  loop: true
-});
