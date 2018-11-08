1d77b5d99b911aa17132ec1bb7b8a8e17944aaec
Jonathan Sanderson <sanderson.jma@gmail.com>
2018-11-08 12:34:27 -0700

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,9 @@ import anime from 'animejs'
 import Rellax from 'rellax'
 
 var rellax = new Rellax('.rellax');
+// var rellax = new Rellax('.content-span--vertical');
+
+// var rellax = new Rellax('.content-span--vertical', {
+//   transform: 'rotate(45deg)',
+//   replace: true
+// });
